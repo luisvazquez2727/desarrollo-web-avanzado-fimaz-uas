4502f79cb95347bd7b2370b74ee2df455369588d
luisosuna1997 <lvgluisjesus@gmail.com>
2026-03-08 17:34:50 -0700

    Agrega clase Admin que hereda de Usuario

---
+++ parcial-1-poo/practica-2/Admin.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'Usuario.php';
+
+// La clase Admin hereda de Usuario
+class Admin extends Usuario {
+    // Método adicional que devuelve el rol
+    public function getRol() {
+        return "Administrador";
+    }
+}
+?>
