2b0f3c309d31b66b52f7e7542c3b862c2afab96e
luisosuna1997 <lvgluisjesus@gmail.com>
2026-03-08 17:33:54 -0700

    Agrega clase Usuario con encapsulamiento

---
+++ parcial-1-poo/practica-2/Usuario.php
@@ -0,0 +1,33 @@
+<?php
+class Usuario {
+    // Atributos privados: solo accesibles dentro de la clase
+    private $nombre;
+    private $correo;
+
+    // Constructor: inicializa nombre y correo al crear el objeto
+    public function __construct($nombre, $correo) {
+        $this->nombre = $nombre;
+        $this->correo = $correo;
+    }
+
+    // Getter para obtener el nombre
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    // Getter para obtener el correo
+    public function getCorreo() {
+        return $this->correo;
+    }
+
+    // Setter para cambiar el nombre
+    public function setNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+
+    // Setter para cambiar el correo
+    public function setCorreo($correo) {
+        $this->correo = $correo;
+    }
+}
+?>
