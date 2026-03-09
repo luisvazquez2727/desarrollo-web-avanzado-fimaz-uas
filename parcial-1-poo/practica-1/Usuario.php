df4251571b4501e7165d6dea6ebce1a73f436404
luisosuna1997 <lvgluisjesus@gmail.com>
2026-03-08 17:16:58 -0700

    mejorda documentacion

--- parcial-1-poo/practica-1/Usuario.php
+++ parcial-1-poo/practica-1/Usuario.php
@@ -1,31 +1,34 @@
 <?php
 class Usuario {
-    // Atributos privados
+    // Atributos privados: solo accesibles dentro de la clase
     private $nombre;
     private $correo;
 
-    // Constructor
+    // Constructor: inicializa nombre y correo al crear el objeto
     public function __construct($nombre, $correo) {
         $this->nombre = $nombre;
         $this->correo = $correo;
     }
 
-    // Métodos getters
+    // Getter para obtener el nombre
     public function getNombre() {
         return $this->nombre;
     }
 
+    // Getter para obtener el correo
     public function getCorreo() {
         return $this->correo;
     }
 
-    // Métodos setters
+    // Setter para cambiar el nombre
     public function setNombre($nombre) {
         $this->nombre = $nombre;
     }
 
+    // Setter para cambiar el correo
     public function setCorreo($correo) {
         $this->correo = $correo;
     }
 }
 ?>
+
